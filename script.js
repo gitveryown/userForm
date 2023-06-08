68283ab113e86728c366f74c2b435b5341f34265
gitveryown <1williambwrgiht@gmail.com>
2023-06-08 14:26:35 -0400

    updated table

--- script.js
+++ script.js
@@ -25,23 +25,7 @@
 
 
 
-
-
-
-// let table = document.querySelector('table')
-
-
-
-
-        var list1 = [];
-		var list2 = [];
-		var list3 = [];
-		var list4 = [];
-        var list5 = [];
-        var list6 = [];
-        var list7 = [];
-        // var list8 = [];
-        // var list9 = [];
+		
 
 		var n = 1;
 		var x = 0;
@@ -49,43 +33,57 @@
 		function AddRow(event){
             
             event.preventDefault()
-			const AddRown = document.getElementById('show');
-			const NewRow = AddRown.insertRow(n);
+			const show = document.getElementById('show');
+			const newRow = show.insertRow(n);
 
-			list1[x] = document.getElementById("fName").value;
-			list2[x] = document.getElementById("lName").value;
-			list3[x] = document.getElementById("phone").value;
-			list4[x] = document.getElementById("email").value;
-            list5[x] = document.getElementById("address").value;
-            list6[x] = document.getElementById("country").value;
-            list7[x] = document.getElementById("state").value;
-            // list8[x] = document.querySelector("gender").value;
-            // list9[x] = document.getElementById("dob").value;
+			var cel1 = newRow.insertCell(0);
+			var cel2 = newRow.insertCell(1);
+			var cel3 = newRow.insertCell(2);
+			var cel4 = newRow.insertCell(3);
+            var cel5 = newRow.insertCell(4);
+			var cel6 = newRow.insertCell(5);
+
+
+			const firstName = document.getElementById("fName").value;
+			const lastName = document.getElementById("lName").value;
+			const phone = document.getElementById("phone").value;
+			const email = document.getElementById("email").value;
+			const streetAddress = document.getElementById("streetAddress").value;
+			// const country = document.getElementById("country").value;
+			const city = document.getElementById("city").value;
+			const state = document.getElementById("state").value;
+			const zipCode = document.getElementById("zip").value ;
+			const address = streetAddress.concat(", ",city,",", state, ",",zipCode);
 
 			
-            var cel1 = NewRow.insertCell(0);
-			var cel2 = NewRow.insertCell(1);
-			var cel3 = NewRow.insertCell(2);
-			var cel4 = NewRow.insertCell(3);
-            var cel5 = NewRow.insertCell(4);
-            var cel6 = NewRow.insertCell(5);
-            var cel7 = NewRow.insertCell(6);
-            // var cel8 = NewRow.insertCell(7);
-            // var cel9 = NewRow.insertCell(8);
+			
+			const editBtn = document.createElement("button");
+			editBtn.innerText = 'Edit';
+			
+			const deleteBtn = document.createElement("button");
+			deleteBtn.innerText = 'Delete';
+			
+			
             
 
-			cel1.innerHTML = list1[x];
-			cel2.innerHTML = list2[x];
-			cel3.innerHTML = list3[x];
-			cel4.innerHTML = list4[x];
-            cel5.innerHTML = list5[x];
-            cel6.innerHTML = list6[x];
-            cel7.innerHTML = list7[x];
-            // cel8.innerHTML = list8[x];
-            // cel9.innerHTML = list9[x];
+		
+			cel1.innerHTML = firstName;
+			cel2.innerHTML = lastName;
+			cel3.innerHTML = phone;
+			cel4.innerHTML = email;
+            cel5.innerHTML = address;
+			cel6.appendChild(editBtn)
+			cel6.appendChild(deleteBtn)
+			
+            editBtn.addEventListener('submit', function(){
+				editRow(row)
+			})
+            deleteBtn.addEventListener('submit', function(){
+				deleteRow(row)
+			})
 
-			n++;
-			x++;
+			// n++;
+			// x++;
             
 		}
 
@@ -93,26 +91,17 @@
         form.addEventListener('submit', AddRow)
 
         
+		// create edit and delete button on action table header
+
+		function editRow(row) {
+			const cells = row.cells;
+			const firstName = cells[0].innerHTML
+			const lastName = cells[1].innerHTML
+			const phone = cells[2].innerHTML
+			const email = cells[3].innerHTML
+			const address = cells[4].innerHTML	
 
 
-// const table = document.getElementById('show')
+			
+		}
 
-
-// function addRow(e) {
-//     e.preventDefault()
-
-//     const data = new FormData(e.target)
-//     const value = Object.fromEntries(data.entries())
-    
-//     const key = Object.keys(value)
-
-//     const output = document.querySelector('.output pre')
-//     output.innerText = JSON.stringify(key, null, 2)
-//     // const row = output.insertRow(()=> Object.key(value))
-//     // console.log(row)
-
-// }
-
-// const form = document.getElementById('contactForm')
-
-// form.addEventListener('submit', addRow)
